eb3c60edcfd96523a991798fd352e3e74a6dbc17
Brian Cort <brian@briancort.com>
2016-09-05 12:08:29 -0400

    Added pagePartials -- moved page template boilerplate to a Handlebars partial

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 var routes = require('./routes/index');
 // var users = require('./routes/users');
 var gallery = require('./routes/gallery');
