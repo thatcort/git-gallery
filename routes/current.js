4de2d66f74b1cb602a5fca1f95e7e953cab9eda5
Brian Cort <brian@briancort.com>
2016-09-20 18:24:18 -0400

    Fix current page rendering

--- routes/current.js
+++ routes/current.js
@@ -6,7 +6,7 @@ const repoUtils = require('../repoUtils');
 
 router.get('/current.html', (req, res, next) => {
 	return repoUtils.repoStatus().then(status => {
-		return res.render('currentPage', status);
+		return res.render('current', status);
 	});
 });
 
